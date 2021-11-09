a9b4779535efec9a3a4186e50808165ad30a2b8d
soufian91 <soufian.msallem@u-psud.fr>
2021-11-09 19:06:13 +0100

    partie 1

--- P1/src/frog/Frog.java
+++ P1/src/frog/Frog.java
@@ -8,21 +8,49 @@ import util.Direction;
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Case lacase;
+	private Direction ladirection;
 
+	public  Frog (Game lapartie){
+		lacase = new Case(game.width/2, 0);
+		ladirection = Direction.up;
+	}
 
 
 	@Override
 	public Case getPosition() {
-		return null;
+		return lacase;
 	}
 
 	@Override
 	public Direction getDirection() {
-		return null;
+		return ladirection;
 	}
 
 	@Override
 	public void move(Direction key) {
+		switch (key){
+			case up:
+				lacase = new Case(getPosition().absc, getPosition().ord + 1 ); // mise a jour de case
+				ladirection = key;
+				break;
+
+			case down:
+				lacase = new Case(getPosition().absc, getPosition().ord - 1 ); // mise a jour de case
+				ladirection = key;
+				break;
+
+			case right:
+				lacase = new Case(getPosition().absc +1, getPosition().ord);
+				ladirection = key;
+				break;
+
+			case left:
+				lacase =  new Case(getPosition().absc -1, getPosition().ord);
+				ladirection = key;
+				break;
+
+		}
 
 	}
 }
